0f1e3435da65ded05c2f44fc7f8d7e95a593a440
Juniper007 <007JenniferReid@gmail.com>
2022-10-24 16:10:41 -0600

    Added Welcome end point

--- server_Project1.js
+++ server_Project1.js
@@ -1,16 +1,70 @@
-import express from 'express'
+import express, { response } from 'express'
 import rl from 'readline-sync'
 
 const app = express()
+app.use(express.json())
+
 const PORT = 4000
 
-app.listen(PORT, function () {
-  console.log(`listening on port ${PORT}`)
+let player1Name
+let player2Name
+let player1Choice
+let player2Choice
+let player1Score
+let player2Score
 
-  // app.get('/', (req, res) => res.send('Hello World!'))
-  // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  //})
+function compare(player1Choice, player2Choice) {
+  player1Choice = player1Choice.toLowerCase()
+  player2Choice = player2Choice.toLowerCase()
+  if (player1Choice === 'rock') {
+    if (player2Choice === 'rock') {
+      return 'Draw'
+    } else if (player2Choice === 'paper') {
+      return 'player2'
+    } else {
+      return 'player1'
+    }
+  } else if (player1Choice === 'paper') {
+    if (player2Choice === 'rock') {
+      return 'player1'
+    } else if (player2Choice === 'paper') {
+      return 'Draw'
+    } else {
+      return 'player2'
+    }
+  } else if (player1Choice === 'scissors') {
+    if (player2Choice === 'rock') {
+      return 'player2'
+    } else if (player2Choice === 'paper') {
+      return 'player1'
+    } else {
+      return 'Draw'
+    }
+  }
+}
 
-  // app.get('/start_dogLife', (req, res) => {
-  //   res.send(player_dogName_dogLife())
-})
+app.get('/welcome', (req, res) =>
+  res.send('Welcome to rock, paper, scissors!\n\
+Player 1: Enter your name'),
+)
+
+// app.post('/player1Name', (req, res) => {
+//  player1Name = req.body.name;
+//  response.send(`Hello ${player1Name},\n\
+//  Player 2: Enter your name`)
+// })
+
+// app.post('/player2Name', (req, res) => {
+//   player2Name = req.body.name;
+//   response.send(`Hello ${player2Name},\n\
+//   Player 1: What is your choice? Rock, Paper, or Scissors?`)
+//  })
+
+//  app.post('/player1Choice', (req, res) => {
+//   player1Choice = req.body.name;
+//   response.send(`Player 2: Enter your name`)
+//  })
+
+// app.listen(PORT, function () {
+//   console.log(`listening on port ${PORT}`)
+// })
